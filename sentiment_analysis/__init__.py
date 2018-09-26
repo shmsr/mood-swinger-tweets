83d2d6e91f86d0a3702d8a9b7a11ffe5aff38c91
Subham Sarkar <sarkar.subhams2@gmail.com>
2018-09-26 11:46:56 +0530

    modified files added embeds

--- sentiment_analysis/__init__.py
+++ sentiment_analysis/__init__.py
@@ -1,31 +1,8 @@
-"""
-Implementation of Mood Detection from tweets.
-
-Scrapes Twitter(currently) for most recent tweets
-by a person (given user handle).
-
-Detects mood/ tone of tweets using IBM Watson Tone Analyser.
-
-Displays mood.
-"""
-
 def remove_non_ascii(text):
-    """
-    Remove Non ascii characters.
-
-    @type   s :  string
-    @param  s :  string to be stripped off non-ascii characters
-    @return   :  non-ascii stripped string
-    """
     return "".join(i for i in text if ord(i) < 128)
 
-def moody(rec_tweet):
-    """
-    Return a json dump of topmost tweet with mood points.
 
-    @type   s          :  string
-    @return            :  json dump
-    """
+def moody(rec_tweet):
     from watson_developer_cloud import ToneAnalyzerV3
     ta_username, ta_password = "e16bef9a-baf8-44f3-8a66-0aba2ed6d40a", "oQSJjrrhv4fQ"
 
@@ -43,56 +20,39 @@ def moody(rec_tweet):
         'application/json')
     return tone_analysis
 
-def isemotional(tone):
-    """
-    Return if tone is an emotional one.
 
-    @type   s    :  string
-    @param  tone :  tone
-    @return      :  boolean true or false
-    """
+def isemotional(tone):
     if tone in ('anger', 'fear', 'joy', 'sadness'):
         return True
-
     return False
 
-def recommend_playlist(rec_tweet):
-    """
-    Returns a recommended YouTube playlist on basis of mood
 
-    @type   s          :  string
-    @return            :  url of recommended playlist
-    """
-    # The service can return results for the following tone IDs:
-    # `anger`, `fear`, `joy`, and `sadness` (emotional tones); `analytical`, `confident`,
-    # and `tentative` (language tones).
+def recommend_playlist(rec_tweet):
     tone_json = moody(rec_tweet)
     tones_list = tone_json.get_result()["document_tone"]["tones"]
 
     mood = ""
+    mood_id = ""
     max_score = -1
     playlist_url = ""
-    
+
     for tone in enumerate(tones_list):
         if isemotional(tone[1]["tone_id"]) and tone[1]["score"] > max_score:
             max_score = tone[1]["score"]
-            mood = tone[1]["tone_id"]
+            mood = tone[1]["tone_name"]
+            mood_id = tone[1]["tone_id"]
 
     if max_score <= 0:
         return -1
 
-    if mood == "anger":
-        playlist_url = "https://www.youtube.com/watch?v=Q1jE25zn8RU"
-    if mood == "fear":
-        playlist_url = "https://www.youtube.com/watch?v=xo1VInw-SKc&list="\
-        	"PLIeiyBOIivZNqYgeJTdamFTXdQwNzDIiD"
-    if mood == "joy":
-        playlist_url = "https://www.youtube.com/watch?v=LjhCEhWiKXk&list="\
-        	"PL1VuYyZcPYIJTP3W_x0jq9olXviPQlOe1"
-    if mood == "sadness":
-        playlist_url = "https://www.youtube.com/watch?v=aJOTlE1K90k&list="\
-        	"PL4QNnZJr8sRPeLgoOL9t4V-18xRAuqe_f"
+    if mood_id == "anger":
+        playlist_url = "https://www.youtube.com/embed/Q1jE25zn8RU"
+    if mood_id == "fear":
+        playlist_url = "https://www.youtube.com/embed/xo1VInw-SKc"
+    if mood_id == "joy":
+        playlist_url = "https://www.youtube.com/embed/LjhCEhWiKXk"
+    if mood_id == "sadness":
+        playlist_url = "https://www.youtube.com/embed/aJOTlE1K90k"
 
-    mood_play = {"mood": mood, "url": playlist_url}
-    print(mood_play)
+    mood_play = {"mood": mood, "url": playlist_url, "json_res": tones_list}
     return mood_play
--- tweet_harvester/__init__.py
+++ tweet_harvester/__init__.py
@@ -10,11 +10,15 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 # These config variables come from 'config.py'
-auth = tweepy.OAuthHandler(app.config['TWITTER_CONSUMER_KEY'], app.config['TWITTER_CONSUMER_SECRET'])
-auth.set_access_token(app.config['TWITTER_ACCESS_TOKEN'], app.config['TWITTER_ACCESS_TOKEN_SECRET'])
+auth = tweepy.OAuthHandler(
+    app.config['TWITTER_CONSUMER_KEY'], app.config['TWITTER_CONSUMER_SECRET'])
+auth.set_access_token(
+    app.config['TWITTER_ACCESS_TOKEN'], app.config['TWITTER_ACCESS_TOKEN_SECRET'])
 tweepy_api = tweepy.API(auth)
 
 # We define our URL route, and the controller to handle requests
+
+
 @app.route('/')
 def hello_world():
     return render_template('home.html', title='Home')
@@ -38,37 +42,37 @@ def get_tweets(username, count):
                 'created_at': t.created_at,
                 'username': username,
                 'headshot_url': t.user.profile_image_url,
-                'name': t.user.name }
+                'name': t.user.name}
                 for t in tweets]
     except tweepy.TweepError as e:
         return 0
 
+
 @app.route('/tweet-harvester/<string:username>')
 @app.route('/tweet-harvester/<string:username>/')
-@app.route('/tweet-harvester/<string:username>/<int:count>')
 def tweets(username, count=5):
     tweets = get_tweets(username, count)
     if tweets:
-        img = tweets[0]['headshot_url'].replace("_normal","")
+        img = tweets[0]['headshot_url'].replace("_normal", "")
         name = tweets[0]['name']
         update = tweets[0]['tweet']
         mood_play = sentiment_analysis.recommend_playlist(update)
-        if mood_play == -1 :
+        if mood_play == -1:
             return render_template('tweets.html',
-                                    tweets=tweets,
-                                    title=username,
-                                    img=img,
-                                    name=name,
-                                    last=update,
-                                    sentiment="Sorry! We couldn't find an emotional tone!")
-        else :
+                                   tweets=tweets,
+                                   title=username,
+                                   img=img,
+                                   name=name,
+                                   last=update,
+                                   sentiment="")
+        else:
             return render_template('tweets.html',
-                                    tweets=tweets,
-                                    title=username,
-                                    img=img,
-                                    name=name,
-                                    last=update,
-                                    sentiment=mood_play["mood"])
+                                   tweets=tweets,
+                                   title=username,
+                                   img=img,
+                                   name=name,
+                                   last=update,
+                                   sentiment=mood_play)
 
     else:
         return render_template('Tweet_404.html'), 404
