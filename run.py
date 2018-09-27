79618072494b1dd7ae8a1604bd0499134936603e
Subham Sarkar <sarkar.subhams2@gmail.com>
2018-09-27 23:55:09 +0530

    ready to be deployed

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from tweet_harvester import app
-import os
+from os import environ
+# port = os.environ['PORT']
 # 'app' originates from the line 'app = Flask(__name__)'
-app.run(host = '0.0.0.0',port = 8080 | os.environ['PORT'])
+app.run(port=environ.get("PORT", 8080))
